ba65533aa8de0a603516a31925719dee0f0f9996
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2026-04-04 01:17:05 -0500

    chore: Eliminación archivo legacy.

--- src/common/value-objects/numero-de-telefono.vo.ts
+++
@@ -1,7 +0,0 @@
-export class PhoneNumber {
-  private static readonly REGEX_E164 = /^\+[1-9]\d{1,14}$/;
-
-  static isValid(value: string): boolean {
-    return PhoneNumber.REGEX_E164.test(value);
-  }
-}
