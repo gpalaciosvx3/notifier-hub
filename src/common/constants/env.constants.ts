22a813f4559f4c13d81819ce1c9b2695708de4a8
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2026-04-07 00:31:31 -0500

    fix: Agregando constantes map.

--- src/common/constants/env.constants.ts
+++ src/common/constants/env.constants.ts
@@ -6,9 +6,19 @@ export class EnvConstants {
   static readonly EMAIL_DEFAULT_PROVIDER  = 'EMAIL_DEFAULT_PROVIDER';
   static readonly SMS_DEFAULT_PROVIDER    = 'SMS_DEFAULT_PROVIDER';
 
-  static readonly REQUERIDAS = [
+  static readonly REQUERIDAS_QUERY: readonly string[] = [
+    EnvConstants.NOTIFICATIONS_TABLE,
+  ];
+  static readonly REQUERIDAS_DLQ: readonly string[] = [
+    EnvConstants.NOTIFICATIONS_TABLE,
+  ];
+  static readonly REQUERIDAS_WORKER: readonly string[] = [
+    EnvConstants.NOTIFICATIONS_TABLE,
+    EnvConstants.SES_SOURCE_EMAIL,
+  ];
+  static readonly REQUERIDAS_ENQUEUE: readonly string[] = [
     EnvConstants.NOTIFICATIONS_TABLE,
     EnvConstants.NOTIFICATIONS_QUEUE_URL,
     EnvConstants.SES_SOURCE_EMAIL,
-  ] as const;
+  ];
 }
