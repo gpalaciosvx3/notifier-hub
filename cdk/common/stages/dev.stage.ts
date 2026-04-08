474f01c7c817cdeee536e9da06d318d5d32dd8cb
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2026-04-07 23:58:36 -0500

    fix: Corrección de obtención de secretos en build.

--- cdk/common/stages/dev.stage.ts
+++ cdk/common/stages/dev.stage.ts
@@ -1,7 +1,7 @@
 import { StageConfig } from '../types/stage-config.types';
 
 export const DevStage: StageConfig = {
-  account: process.env.CDK_DEFAULT_ACCOUNT ?? '',
+  account: process.env.CDK_DEFAULT_ACCOUNT || '000000000000',
   region:  process.env.CDK_DEFAULT_REGION  ?? 'us-east-1',
   sesSourceEmail: process.env.SES_SOURCE_EMAIL ?? '',
 };
